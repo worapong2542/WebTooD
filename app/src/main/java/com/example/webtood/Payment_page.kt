c32861cce4ab36410ae0a59997871ead1d3c239c
unknown <1810511101030@live4.utcc.ac.th>
2021-03-08 13:42:09 +0700

    first commit

---
+++ app/src/main/java/com/example/webtood/Payment_page.kt
@@ -0,0 +1,11 @@
+package com.example.webtood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Payment_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_page)
+    }
+}
